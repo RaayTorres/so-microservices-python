385a08d5b5d8bb37921b98e2d175b59db09aa3ca
RaayTorres <ray0395@hotmail.com.ar>
2017-04-21 14:26:04 -0500

    Add files via upload

---
+++ A00309856/05_flash_app/users_commands.py
@@ -0,0 +1,32 @@
+from subprocess import Popen, PIPE
+
+def get_all_users():
+  grep_process = Popen(["grep","/bin/bash","/etc/passwd"], stdout=PIPE, stderr=PIPE)
+  user_list = Popen(["awk",'-F',':','{print $1}'], stdin=grep_process.stdout, stdout=PIPE, stderr=PIPE).communicate()[0].split('\n')
+  return filter(None,user_list)
+
+def add_user(username,password):
+  add_process = Popen(["sudo","adduser","--password",password,username], stdout=PIPE, stderr=PIPE)
+  add_process.wait()
+  return True if username in get_all_users() else False
+
+def remove_user(username):
+  vip = ["operativos","jenkins","postgres","root"]
+  if username in vip:
+    return True
+  else:
+    remove_process = Popen(["sudo","userdel","-r",username], stdout=PIPE, stderr=PIPE)
+    remove_process.wait()
+    return False if username in get_all_users() else True
+
+
+def recently_logged():
+   reciente  = Popen(["last"], stdout=PIPE, stderr=PIPE).communicate()[0].split('\n')
+   return reciente
+
+   
+ def history_users(username):
+ 
+  list = Popen (["cat","/home/"+username+"/.bash_history"], stdout=PIPE, stderr=PIPE).communicate()[0].split('\n')
+  return filter(None, list)
+ 
